1c17ac2b0d5ff4139e37fabdcc218ef4c68212d1
renaulttjandera <matahari16888@gmail.com>
2021-05-20 18:54:17 +0700

    fixing details

--- application/views/post/details.php
+++ application/views/post/details.php
@@ -2,6 +2,6 @@
         <p class="lead"><?= date('D, d M Y', $post['date_created']); ?></p>
         <h1 class="display-4"><?= $post['title']; ?></h1>
         <p class="lead">Posted by: <?= $post['author'] ?></p>
-        <p><?= $post['content']; ?></p>
+        <p style="word-wrap: break-word;"><?= $post['content']; ?></p>
         <a href="<?= base_url('post'); ?>" class="btn btn-danger mb-5">Back</a>
     </div>
--- application/views/post/index.php
+++ application/views/post/index.php
@@ -10,17 +10,32 @@
         <h1 class="display-4">Latest Post</h1>
         <div class="row mt-3 mb-5">
         <?php foreach ($posts as $p): ?>
+        
             <div class="col-md-4 mb-3">
                 <div class="card" style="width: 18rem;">
                     <img src="<?= base_url('assets/') ?>default-product.png" class="card-img-top">
                     <div class="card-body">
                         <h5 class="card-title"><?= $p['title']; ?></h5>
-                        <p class="card-text"><?= implode(' ', array_slice(explode(' ', $p['content']), 0, 20)) ?> ...</p>
+                        <p class="card-text"><?= limit_content($p['content'], 100); ?></p>
                         <a href="<?= base_url('post/details?id=') . $p['id']; ?>" class="btn btn-danger">Read More</a>
                     </div>
                 </div>
             </div>
         <?php endforeach; ?>
+        <?php 
+        function limit_content($x, $length)
+        {
+          if(strlen($x)<=$length)
+          {
+            echo $x;
+          }
+          else
+          {
+            $y=substr($x,0,$length) . ' ...';
+            echo $y;
+          }
+        }    
+        ?>
         </div>
     </div>
     
